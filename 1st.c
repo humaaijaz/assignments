b0aabfffab2d6d4aba631ba383c6efb1996c8be0
humaaijaz <146563055+humaaijaz@users.noreply.github.com>
2023-12-09 18:46:26 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, max, min;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
